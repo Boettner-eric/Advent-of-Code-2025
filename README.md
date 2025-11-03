5b9b4a8dd7372e222ae665b5ac4770a233344a48
Eric Boettner <boettner.eric@gmail.com>
2025-11-03 10:00:00 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Advent of Code 2025
+
+This is my attempt at the [Advent of Code 2025](https://adventofcode.com/2025) challenge.
+
+## Usage
+- `mix day.gen <number> <name>` will create files and tests for each challenge
+- `mix test test/day<number>.exs` will run the tests for a given day
---
+++ lib/advent_of_code.ex
@@ -0,0 +1,36 @@
+defmodule AdventOfCode do
+  @moduledoc """
+  This module contains functions to help with the Advent of Code challenge.
+  """
+
+  @doc """
+  Reads a file and returns a list of lines.
+
+  ## Examples
+
+      iex> AdventOfCode.read_lines("example.txt")
+      ["this is the first list", "it continues on the next line", "and this is the third line"]
+
+  """
+  def read_lines(filename) do
+    {:ok, input} = File.read(filename)
+
+    String.split(input, "\n")
+  end
+
+  @doc """
+  Reads a file and returns its whole contents as a string.
+  This is useful for reading odd inputs that need to be parsed differently than just lines.
+
+  ## Examples
+
+      iex> AdventOfCode.read_blob("example.txt")
+      "this is the first list\nit continues on the next line\nand this is the third line"
+
+  """
+  def read_blob(filename) do
+    {:ok, input} = File.read(filename)
+
+    input
+  end
+end
